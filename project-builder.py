1731b1e857d7766405bd64c3a924481930afa2ff
zbuhrer <zbuhrer@gmail.com>
2024-08-23 17:46:39 -0500

    modified:   project-builder.py

--- project-builder.py
+++ project-builder.py
@@ -7,7 +7,7 @@ with open('project-structure.json', 'r') as f:
 def create_project_structure(structure, path=""):
     for key, value in structure.items():
         if isinstance(value, dict) and value.get("type") == "directory":
-            os.makedirs(f"{path}/{key}", exist_ok=True)
+            os.makedirs(f"./{path}/{key}", exist_ok=True)
             create_project_structure(value["children"], f"{path}/{key}")
         elif value.get("type") == "file":
             open(f"{path}/{key}", "a").close()
