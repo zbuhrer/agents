81a3d3cbb69536f8037a23f32d5d7b3211c0f3bb
zbuhrer <zbuhrer@gmail.com>
2024-08-23 18:54:22 -0500

    create: core system logs, auth config

--- project-builder.py
+++ project-builder.py
@@ -5,11 +5,15 @@ with open('project-structure.json', 'r') as f:
     project_structure = json.load(f)
 
 def create_project_structure(structure, path=""):
-    for key, value in structure.items():
-        if isinstance(value, dict) and value.get("type") == "directory":
-            os.makedirs(f"./{path}/{key}", exist_ok=True)
-            create_project_structure(value["children"], f"{path}/{key}")
-        elif value.get("type") == "file":
-            open(f"{path}/{key}", "a").close()
+    if isinstance(structure, list):
+        for item in structure:
+            create_project_structure(item, path)
+    elif isinstance(structure, dict):
+        for key, value in structure.items():
+            if isinstance(value, dict) and value.get("type") == "directory":
+                os.makedirs(f"./{path}/{key}", exist_ok=True)
+                create_project_structure(value["children"], f"{path}/{key}")
+            elif value.get("type") == "file":
+                open(f"./{path}/{key}", "a").close()
 
 create_project_structure(project_structure)
